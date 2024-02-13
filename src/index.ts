e74c4f4b39dbb1488dabb339d12170638ee53577
Ranko Orlic <ranko@aquila-solutions.com>
2024-02-13 18:00:46 +0100

    chore: follow only structure

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ import minimist from 'minimist';
 import { Parser, Store } from 'n3';
 import fetch from 'node-fetch';
 import {SortedQueue} from 'sorted-queue';
-import CacheControl from 'cache-control-parser';
-const { parse } = CacheControl;
+// import CacheControl from 'cache-control-parser';
+// const { parse } = CacheControl;
 
 const args = minimist(process.argv.slice(2));
 const silent: boolean = (/true/i).test(args['silent'] || 'true');
-const mimeType = args['mime-type'] || 'application/n-quads';
-const pollInterval = Number.parseInt(args['poll-interval'] || '1000');
+const mimeType = 'application/n-quads';
+const pollInterval = Number.parseInt(args['poll-interval'] || '100');
 const viewUrl = args['follow'];
 
 function showUsage() {
-  console.error('usage: node ./dist/index.js --follow=<follow> [--silent=true|false] [--mime-type=<mime-type>] [--poll-interval=<millis>]');
+  console.error('usage: node ./dist/index.js --follow=<follow> [--silent=true|false] [--poll-interval=<millis>]');
   console.error('\t--follow=<follow>\tview URL of LDES to list all existing fragments and keep following for new ones');
   console.error('\t--silent=true|false\tdefaults to true (shows no debug info)');
-  console.error('\t--mime-type=<mime-type>\tmime type to use for fetching LDES, defaults to application/n-quads');
-  console.error('\t--poll-interval=<millis>\tpolling interval in milliseconds (to check queue of fragments to visit), defaults to 1000');
+  console.error('\t--poll-interval=<millis>\tpolling interval in milliseconds (to check queue of fragments to visit), defaults to 100');
 }
 
 function sleep(ms: number) {
@@ -32,17 +31,20 @@ async function visit(url: string, queue: SortedQueue<TimedUrl>, visited: {[key: 
   }
 
   const headers = response.headers;
-  const cacheControl = parse(headers.get('cache-control') || '');
-  const ttl = cacheControl["s-maxage"] ?? cacheControl["max-age"] ?? 0;
-  const expires = new Date(Date.now() + ttl * 1000);
+  // const cacheControl = parse(headers.get('cache-control') || '');
+  // const ttl = cacheControl["s-maxage"] ?? cacheControl["max-age"] ?? 0;
+  // const expires = new Date(Date.now() + ttl * 1000);
 
   const parser = new Parser({ format: headers.get('content-type') || '' });
   const body = await response.text();
   const model = new Store(parser.parse(body));
-
+  
   const fragmentId = model.getSubjects(null, 'https://w3id.org/tree#Node', null).shift()?.value;
   if (fragmentId)
   {
+    const memberCount = model.getObjects(null, 'https://w3id.org/tree#member', null).length;
+    if (memberCount > 0) return; // it's a data node
+
     if (!visited[fragmentId]) console.info(fragmentId);
     visited[fragmentId] = true;
 
@@ -50,7 +52,7 @@ async function visit(url: string, queue: SortedQueue<TimedUrl>, visited: {[key: 
     const unvisited = relations.filter(x => !visited[x]);
     unvisited.forEach(x => queue.push({at: undefined, url: x}));
 
-    if (!cacheControl.immutable) queue.push({at: expires, url: fragmentId});
+    // if (!cacheControl.immutable) queue.push({at: expires, url: fragmentId});
   }
 }
 
